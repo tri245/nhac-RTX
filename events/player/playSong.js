2368b410a5acc742ec60d872ff4940c8d21d5341
Tri_nho <76763671+tri245@users.noreply.github.com>
2024-03-20 15:02:30 +0700

    Update playSong.js

--- events/player/playSong.js
+++ events/player/playSong.js
@@ -7,14 +7,16 @@ module.exports = async (client, queue, song) => {
     if (queue?.textChannel) {
       const embed = new EmbedBuilder()
       .setAuthor({
-        name: 'Currently playing a Track',
+        name: 'Hiện đang phát một Bản nhạc',
         iconURL: 'https://cdn.discordapp.com/attachments/1140841446228897932/1144671132948103208/giphy.gif', 
         url: 'https://discord.gg/FUEHs7RCqz'
     })
-    .setDescription(`\n ‎ \n▶️ **Details :** **${song?.name}**\n▶️ **Enjoy the Ultimate Music Experience. ** \n▶️ **If link breaks playback try to give query.**`)
+    .setImage(`https://cdn.discordapp.com/attachments/1004341381784944703/1165201249331855380/RainbowLine.gif?ex=654f37ba&is=653cc2ba&hm=648a2e070fab36155f4171962e9c3bcef94857aca3987a181634837231500177&`);    
+    .setDescription(`\n ‎ \n▶️ **Chi tiết :** **${song?.name}**\n▶️ **Tận hưởng trải nghiệm âm nhạc đỉnh cao. ** \n▶️ **Nếu liên kết bị hỏng khi phát lại, hãy thử đưa ra truy vấn.**`)
 .setImage(queue.songs[0].thumbnail)
-    .setColor('#FF0000')
-    .setFooter({ text: 'More info - Use /help command [RTX GAMING]' });
+    .setColor('#00FF00')
+    .setImage('https://antimatter.vn/wp-content/uploads/2022/04/hinh-nen-dong-powerpoint-chat-luong-cao.gif')
+    .setFooter({ text: 'Thông tin thêm - Sử dụng lệnh /help [TriNho]' });
      
       queue?.textChannel?.send({ embeds: [embed] }).catch(e => { });
     }
