bf031c972f66195f88f2c6a50f778dec1d3122b5
Tri_nho <76763671+tri245@users.noreply.github.com>
2024-03-20 15:09:48 +0700

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -17,14 +17,14 @@ module.exports = {
     try {
       const queue = client.player.getQueue(interaction.guild.id);
       if (!queue || !queue.playing) {
-        return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true });
+        return interaction.reply({ content: '⚠️ Không phát nhạc!!', ephemeral: true });
       }
 
       const vol = parseInt(interaction.options.getInteger('volume'));
 
       if (!vol) {
         return interaction.reply({
-          content: `Current volume: **${queue.volume}** 🔊\nTo change the volume, type a number between \`1\` and \`${maxVol}\`.`,
+          content: `Current volume: **${queue.volume}** 🔊\nĐể thay đổi âm lượng, hãy nhập một số ở giữa \`1\` and \`${maxVol}\`.`,
           ephemeral: true
         });
       }
@@ -50,11 +50,11 @@ module.exports = {
         iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157528025739563088/5657-volume-icon.png?ex=6518ef7b&is=65179dfb&hm=1797c2830537a28b5c6a57564517cc509146d02383a69fb4239d7b5d55aceeed&', 
         url: 'https://discord.gg/FUEHs7RCqz'
     })
-          .setDescription(`**Adjusting Volume : ** **${vol}/${maxVol}**`);
+          .setDescription(`**Điều chỉnh âm lượng : ** **${vol}/${maxVol}**`);
 
         return interaction.reply({ embeds: [embed] });
       } else {
-        return interaction.reply({ content: '❌ Something went wrong while changing the volume.', ephemeral: true });
+        return interaction.reply({ content: '❌ Đã xảy ra lỗi khi thay đổi âm lượng.', ephemeral: true });
       }
     } catch (e) {
       console.error(e);
