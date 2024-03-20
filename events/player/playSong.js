71a21cbe1a1733062062e1c83c60bf110f8350fe
Tri_nho <76763671+tri245@users.noreply.github.com>
2024-03-20 15:30:04 +0700

    Update playSong.js

--- events/player/playSong.js
+++ events/player/playSong.js
@@ -11,10 +11,12 @@ module.exports = async (client, queue, song) => {
         iconURL: 'https://cdn.discordapp.com/attachments/1140841446228897932/1144671132948103208/giphy.gif', 
         url: 'https://discord.gg/FUEHs7RCqz'
     })
+    .setImage(`https://cdn.discordapp.com/attachments/1004341381784944703/1165201249331855380/RainbowLine.gif?ex=654f37ba&is=653cc2ba&hm=648a2e070fab36155f4171962e9c3bcef94857aca3987a181634837231500177&`); 
     .setDescription(`\n ‎ \n▶️ **Chi tiết :** **${song?.name}**\n▶️ **Tận hưởng trải nghiệm âm nhạc đỉnh cao. ** \n▶️ **Nếu liên kết bị hỏng khi phát lại, hãy thử đưa ra truy vấn.**`)
 .setImage(queue.songs[0].thumbnail)
     .setColor('#FF0000')
-    .setFooter({ text: 'More info - Use /help command [RTX GAMING]' });
+        
+    .setFooter({ text: 'Thông tin thêm - Sử dụng lệnh /help [Trinho]' });
      
       queue?.textChannel?.send({ embeds: [embed] }).catch(e => { });
     }
