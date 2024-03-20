e252d802f7ad15218bda793609982fab7c06bf3f
Tri_nho <76763671+tri245@users.noreply.github.com>
2024-03-20 15:06:44 +0700

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -26,7 +26,7 @@ module.exports = {
     try {
       const queue = client.player.getQueue(interaction.guild.id);
       if (!queue || !queue.playing) {
-        return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true });
+        return interaction.reply({ content: '⚠️ Không phát nhạc!!', ephemeral: true });
       }
 
       queue.stop(interaction.guild.id);
@@ -38,7 +38,7 @@ module.exports = {
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157305318255116400/pngtree-vector-stop-icon-png-image_4233262.jpg?ex=65182011&is=6516ce91&hm=d5a8ca6010716bae836b025f8d36557a95f14c13a705f65eb09a54161649c795&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-        .setDescription('**The journey stops, but the rhythm lives on.**')
+        .setDescription('**Cuộc hành trình dừng lại, nhưng nhịp điệu vẫn tiếp tục.**')
         
 
       return interaction.reply({ embeds: [embed] });
