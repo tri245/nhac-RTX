1b590f2386117d119250f958ea2fc585d3289c26
Tri_nho <76763671+tri245@users.noreply.github.com>
2024-03-20 15:24:07 +0700

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -34,7 +34,7 @@ module.exports = {
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157310253520662638/2443-iconperson.png?ex=651824aa&is=6516d32a&hm=0becc4a0fda01e5a02a63cf098db30c287e60a474f8d2da4ddeae7f47d98a5a3&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-            .setDescription(`__**About me**__:\n\n ▶️ Bản thân tôi là Ngân hay còn gọi là Ngân 3 cộng. Tôi là một con đĩ đứng đường. Tôi thích chơi game, xem phim sex để phát triển kinh nghiệm cho bản thân tôi.Tôi nhận chỉ nhận đi khách. Bạn sẽ liên hệ tôi được tôi phản hồi trên nền tảng FB nhanh hơn các phương tiện giao tiếp khác. Cứ tự nhiên liên lạc cho tôi kiếm tiền nha !\n YouTube : ❤️ [FB](${youtubeLink})\n Instagram : 💙 [FB](${InstagramLink})`)
+            .setDescription(`__**About me**__:\n\n ▶️ Bản thân tôi là Ngân hay còn gọi là Ngân 3 cộng. Tôi là một con đĩ đứng đường. Tôi thích chơi game, xem phim sex để phát triển kinh nghiệm cho bản thân tôi.Tôi nhận chỉ nhận đi khách. Bạn sẽ liên hệ tôi được tôi phản hồi trên nền tảng FB nhanh hơn các phương tiện giao tiếp khác. Cứ tự nhiên liên lạc cho tôi kiếm tiền nha !\n Link : ❤️ [FB](${youtubeLink})\n link : 💙 [FB](${InstagramLink})`)
             .setTimestamp();
       interaction.reply({ embeds: [embed] }).catch(e => {});
 
