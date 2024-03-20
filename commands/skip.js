c7023770a3e254ab280d6458c40004f37191c39a
Tri_nho <76763671+tri245@users.noreply.github.com>
2024-03-20 15:08:10 +0700

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -31,27 +31,27 @@ module.exports = {
     try {
 
       const queue = client.player.getQueue(interaction.guild.id);
-      if (!queue || !queue.playing) return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true }).catch(e => { })
+      if (!queue || !queue.playing) return interaction.reply({ content: '⚠️ Không phát nhạc!!', ephemeral: true }).catch(e => { })
 
       let number = interaction.options.getNumber('number');
       if (number) {
-        if (!queue.songs.length > number) return interaction.reply({ content: '⚠️ Exceeded current no of songs', ephemeral: true }).catch(e => { })
-        if (isNaN(number)) return interaction.reply({ content: '⚠️ Invalid Number', ephemeral: true }).catch(e => { })
-        if (1 > number) return interaction.reply({ content: '⚠️ Invalid Number', ephemeral: true }).catch(e => { })
+        if (!queue.songs.length > number) return interaction.reply({ content: '⚠️ Đã vượt quá số lượng bài hát hiện tại', ephemeral: true }).catch(e => { })
+        if (isNaN(number)) return interaction.reply({ content: '⚠️ Số không hợp lệ', ephemeral: true }).catch(e => { })
+        if (1 > number) return interaction.reply({ content: '⚠️ Số không hợp lệ', ephemeral: true }).catch(e => { })
 
         try {
         let old = queue.songs[0];
         await client.player.jump(interaction, number).then(song => {
-          return interaction.reply({ content: `⏯️ Skipped : **${old.name}**` }).catch(e => { })
+          return interaction.reply({ content: `⏯️ Đã bỏ qua : **${old.name}**` }).catch(e => { })
         })
       } catch(e){
-        return interaction.reply({ content: '❌ Queue is empty!!', ephemeral: true }).catch(e => { })
+        return interaction.reply({ content: '❌ Hàng đợi trống!!', ephemeral: true }).catch(e => { })
       }
       } else {
 try {
   const queue = client.player.getQueue(interaction.guild.id);
   if (!queue || !queue.playing) {
-    return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true });
+    return interaction.reply({ content: '⚠️ Không phát nhạc!!', ephemeral: true });
   }
 
   let old = queue.songs[0];
@@ -64,12 +64,12 @@ try {
       iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157269773118357604/giphy.gif?ex=6517fef6&is=6516ad76&hm=f106480f7d017a07f75d543cf545bbea01e9cf53ebd42020bd3b90a14004398e&',
       url: 'https://discord.gg/FUEHs7RCqz'
     })
-    .setDescription(success ? ` **SKIPPED** : **${old.name}**` : '❌ Queue is empty!')
+    .setDescription(success ? ` **BỎ QUA** : **${old.name}**` : '❌ Hàng đợi trống!')
     .setTimestamp();
 
   return interaction.reply({ embeds: [embed] });
 }catch (e) {
-          return interaction.reply({ content: '❌ Queue is empty!!', ephemeral: true }).catch(e => { })
+          return interaction.reply({ content: '❌ Hàng đợi trống!!', ephemeral: true }).catch(e => { })
         }
       }
 
