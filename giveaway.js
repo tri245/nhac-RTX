65bd5a8413f91ac2bac32eb45f493ac5046a90c9
Tri_nho <76763671+tri245@users.noreply.github.com>
2024-03-20 20:30:35 +0700

    Update giveaway.js

--- giveaway.js
+++ giveaway.js
@@ -1,5 +1,7 @@
 // Dependencies
-const { ApplicationCommandOptionType, PermissionsBitField: { Flags } } = require('discord.js'),
+const { ApplicationCommandOptionType, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+
+
 	Command = require('../../structures/Command.js');
 
 /**
