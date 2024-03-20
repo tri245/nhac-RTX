da2953d6e9445a8b72b022da1d23c504a7a01863
Tri_nho <76763671+tri245@users.noreply.github.com>
2024-03-20 15:57:53 +0700

    Update database.js

--- commands/database.js
+++ commands/database.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.join(__dirname, '../data/database.db');
-const db = new sqlite3.Database(dbPath);
+const dbPath = path.join(__dirname, '../data/database.DB');
+const db = new sqlite3.Database(DBPath);
 
